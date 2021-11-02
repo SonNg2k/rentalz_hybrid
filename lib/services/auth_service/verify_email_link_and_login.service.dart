32341d398f080f46b842a98c40df1386632a6224
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-11-02 22:26:39 +0700

    👔 (util) verify email auth link and log the user in if it is valid

---
+++ lib/services/auth_service/verify_email_link_and_login.service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:rentalz/alert_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<User?> verifyEmailLinkAndLogin(String authLinkSentToEmail) async {
+  final auth = FirebaseAuth.instance;
+  if (!auth.isSignInWithEmailLink(authLinkSentToEmail)) return null;
+
+  /// Retrieve the email from wherever you stored it.
+  final prefs = await SharedPreferences.getInstance();
+  final emailToAuthViaLink = prefs.getString('emailToAuthViaLink');
+  if (emailToAuthViaLink == null) return null;
+
+  try {
+    // The client SDK will parse the code from the link for you.
+    final identityInfo = await auth.signInWithEmailLink(
+        email: emailToAuthViaLink, emailLink: authLinkSentToEmail);
+    debugPrint('Successfully signed in with email link!');
+    AlertService.showEphemeralSnackBar('Signed in successfully ✅');
+    // Additional user profile info *not* available via:
+    // identityInfo.additionalUserInfo.profile == null
+    // You can check if the user is new or existing:
+    // identityInfo.additionalUserInfo.isNewUser;
+    return identityInfo.user;
+  } catch (onError) {
+    String extraMsg = '';
+    final error = onError as FirebaseAuthException;
+    if (error.code == 'invalid-action-code') {
+      extraMsg = '. The link is incorrect, expired, or has already been used.';
+    }
+    AlertService.showEphemeralSnackBar(
+        'Error signing in with email link$extraMsg');
+  }
+}
