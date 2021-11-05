f6f4e518f13afa926e011f392d5b6c88832d6401
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-11-05 20:59:31 +0700

    🏷️ add type for apartment

---
+++ lib/models/apartment_model.dart
@@ -0,0 +1,116 @@
+/// The technique that I use below to convert Dart native data structures to
+/// and from JSON is from here:
+/// https://flutter.dev/docs/development/data-and-backend/json
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'apartment_model.g.dart';
+
+enum ApartmentType {
+  studio,
+  duplex,
+  triplex,
+  garden,
+  loft,
+  penthouse,
+  micro,
+  walkUp,
+  basement,
+  coOp,
+  highRise,
+  midRise,
+  lowRise,
+}
+
+enum ComfortLevel {
+  furnished,
+  semiFurnished,
+  unfurnished,
+}
+
+@JsonSerializable(anyMap: true, explicitToJson: true)
+class ApartmentModel {
+  ApartmentModel({
+    required this.name,
+    required this.reporterName,
+    required this.formattedAddress,
+    required this.addressComponents,
+    required this.type,
+    required this.comfortLevel,
+    required this.monthlyRent,
+    required this.nBedrooms,
+    this.notes,
+    required this.createdAt,
+  });
+
+  final String name;
+
+  @JsonKey(name: 'reporter_name')
+  final String reporterName;
+
+  @JsonKey(name: 'formatted_address')
+  final String formattedAddress;
+
+  @JsonKey(name: 'address_components')
+  final AddressComponents addressComponents;
+
+  final ApartmentType type;
+
+  @JsonKey(name: 'comfort_level')
+  final ComfortLevel comfortLevel;
+
+  @JsonKey(name: 'monthly_rent')
+  final int monthlyRent;
+
+  @JsonKey(name: 'n_bedrooms')
+  final int nBedrooms;
+
+  final String? notes;
+
+  @JsonKey(
+    name: 'created_at',
+    fromJson: _fromJsonTimestamp,
+    toJson: _toJsonTimestamp,
+  )
+  final Timestamp createdAt;
+
+  factory ApartmentModel.fromJson(Map<String, dynamic> json) =>
+      _$ApartmentModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ApartmentModelToJson(this);
+
+  static Timestamp _fromJsonTimestamp(Timestamp ts) => ts;
+  static Timestamp _toJsonTimestamp(Timestamp ts) => ts;
+}
+
+/// The address components are based on this repo
+/// https://github.com/daohoangson/dvhcvn
+@JsonSerializable(anyMap: true, explicitToJson: true)
+class AddressComponents {
+  AddressComponents({
+    required this.level1Id,
+    required this.level2Id,
+    required this.level3Id,
+    required this.route,
+  });
+
+  /// The unique code for the province or city.
+  @JsonKey(name: 'level1_id')
+  final String level1Id;
+
+
+  /// The unique code for the city, district, huyện, or thị xã.
+  @JsonKey(name: 'level2_id')
+  final String level2Id;
+
+  /// The unique code for ward, xã, or thị trấn.
+  @JsonKey(name: 'level3_id')
+  final String level3Id;
+
+  final String route;
+
+  factory AddressComponents.fromJson(Map<String, dynamic> json) =>
+      _$AddressComponentsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AddressComponentsToJson(this);
+}
---
+++ lib/models/apartment_model.g.dart
@@ -0,0 +1,73 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'apartment_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApartmentModel _$ApartmentModelFromJson(Map json) => ApartmentModel(
+      name: json['name'] as String,
+      reporterName: json['reporter_name'] as String,
+      formattedAddress: json['formatted_address'] as String,
+      addressComponents: AddressComponents.fromJson(
+          Map<String, dynamic>.from(json['address_components'] as Map)),
+      type: $enumDecode(_$ApartmentTypeEnumMap, json['type']),
+      comfortLevel: $enumDecode(_$ComfortLevelEnumMap, json['comfort_level']),
+      monthlyRent: json['monthly_rent'] as int,
+      nBedrooms: json['n_bedrooms'] as int,
+      notes: json['notes'] as String?,
+      createdAt:
+          ApartmentModel._fromJsonTimestamp(json['created_at'] as Timestamp),
+    );
+
+Map<String, dynamic> _$ApartmentModelToJson(ApartmentModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'reporter_name': instance.reporterName,
+      'formatted_address': instance.formattedAddress,
+      'address_components': instance.addressComponents.toJson(),
+      'type': _$ApartmentTypeEnumMap[instance.type],
+      'comfort_level': _$ComfortLevelEnumMap[instance.comfortLevel],
+      'monthly_rent': instance.monthlyRent,
+      'n_bedrooms': instance.nBedrooms,
+      'notes': instance.notes,
+      'created_at': ApartmentModel._toJsonTimestamp(instance.createdAt),
+    };
+
+const _$ApartmentTypeEnumMap = {
+  ApartmentType.studio: 'studio',
+  ApartmentType.duplex: 'duplex',
+  ApartmentType.triplex: 'triplex',
+  ApartmentType.garden: 'garden',
+  ApartmentType.loft: 'loft',
+  ApartmentType.penthouse: 'penthouse',
+  ApartmentType.micro: 'micro',
+  ApartmentType.walkUp: 'walkUp',
+  ApartmentType.basement: 'basement',
+  ApartmentType.coOp: 'coOp',
+  ApartmentType.highRise: 'highRise',
+  ApartmentType.midRise: 'midRise',
+  ApartmentType.lowRise: 'lowRise',
+};
+
+const _$ComfortLevelEnumMap = {
+  ComfortLevel.furnished: 'furnished',
+  ComfortLevel.semiFurnished: 'semiFurnished',
+  ComfortLevel.unfurnished: 'unfurnished',
+};
+
+AddressComponents _$AddressComponentsFromJson(Map json) => AddressComponents(
+      level1Id: json['level1_id'] as String,
+      level2Id: json['level2_id'] as String,
+      level3Id: json['level3_id'] as String,
+      route: json['route'] as String,
+    );
+
+Map<String, dynamic> _$AddressComponentsToJson(AddressComponents instance) =>
+    <String, dynamic>{
+      'level1_id': instance.level1Id,
+      'level2_id': instance.level2Id,
+      'level3_id': instance.level3Id,
+      'route': instance.route,
+    };
