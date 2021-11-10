22f0f0a70b5f0209eac1b6771262544ab847a356
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-11-10 16:19:53 +0700

    ✏️ change 'text' to 'value' and 'errorText' to 'labelText'

--- lib/utils/data_validator.dart
+++ lib/utils/data_validator.dart
@@ -3,21 +3,21 @@
 class DataValidator {
   DataValidator._();
 
-  static String? required<T>(T? value, {String? errorText}) {
-    return (value == null) ? (errorText ?? 'This field is required') : null;
+  static String? required<T>(T? value, {String? labelText}) {
+    return (value == null) ? '${labelText ?? 'This field'} is required' : null;
   }
 
-  static String? textRequired(String? text, {String? errorText}) {
-    final errMsg = errorText ?? 'Text is required';
-    if (text == null) return errMsg;
-    if (text.trim().isEmpty) return errMsg;
+  static String? textRequired(String? value, {String? labelText}) {
+    final errMsg = '${labelText ?? 'This field'} is required';
+    if (value == null) return errMsg;
+    if (value.trim().isEmpty) return errMsg;
   }
 
-  static String? fullnameValid(String? text) {
-    if (text == null || text.trim().isEmpty) {
+  static String? fullnameValid(String? value) {
+    if (value == null || value.trim().isEmpty) {
       return 'Fullname is required';
     }
-    if (text.trim().length < 4 || text.trim().length > 64) {
+    if (value.trim().length < 4 || value.trim().length > 64) {
       return 'Name must contain between 4 and 64 characters';
     }
 
@@ -27,36 +27,36 @@ class DataValidator {
       unicode: true,
       caseSensitive: false,
     );
-    if (!_fullnameRegex.hasMatch(text)) {
+    if (!_fullnameRegex.hasMatch(value)) {
       return 'Please remove invalid characters from your name';
     }
   }
 
   static String? lengthRequired(
-    String? text, {
-    String? fieldName,
+    String? value, {
+    String? labelText,
     required int minLength,
     required int maxLength,
   }) {
     assert(minLength > 0, 'Min length must be a postive number');
     assert(maxLength > 0, 'Max length must be a postive number');
-    if (text == null || text.trim().isEmpty) {
-      return '${fieldName ?? 'Text'} is required';
+    if (value == null || value.trim().isEmpty) {
+      return '${labelText ?? 'This field'} is required';
     }
-    if (text.length < minLength || text.length > maxLength) {
-      return '${fieldName ?? 'Text'} must contain between $minLength and $maxLength characters';
+    if (value.length < minLength || value.length > maxLength) {
+      return '${labelText ?? 'This field'} must contain between $minLength and $maxLength characters';
     }
   }
 
-  static String? emailAddressValid(String? email) {
-    if (email == null || email.trim().isEmpty) return 'Email is required';
-    if (email.trim().length < 5 || email.trim().length > 50) {
+  static String? emailAddressValid(String? value) {
+    if (value == null || value.trim().isEmpty) return 'Email is required';
+    if (value.trim().length < 5 || value.trim().length > 50) {
       return 'Email must contain between 5 and 50 characters';
     }
     final emailRegex = RegExp(
       r'''^(([^<>()\[\]\\.,;:\s-@#$!%^&*+=_/`?{}|'"]+(\.[^<>()\[\]\\.,;:\s-@_!#$%^&*()=+/`?{}|'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$''',
       caseSensitive: false,
     );
-    if (!emailRegex.hasMatch(email)) return 'Email is not valid';
+    if (!emailRegex.hasMatch(value)) return 'Email is not valid';
   }
 }
